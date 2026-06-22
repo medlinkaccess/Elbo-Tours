036326d58f15f2eba655a0484e3a2fb692e062a7
medlinkaccess <kasif19601@gmail.com>
2026-06-22 21:06:24 +0100

    Fix duplicate dynamic export in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,4 +1,6 @@
-import { MetadataRoute } from 'next';
+﻿import { MetadataRoute } from 'next';
+
+export const dynamic = 'force-dynamic';
 
 export default function robots(): MetadataRoute.Robots {
   return {
